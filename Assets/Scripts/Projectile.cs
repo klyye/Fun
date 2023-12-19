37715f02fffa7574c58f7a617da7b8b6201a8d44
Kyle Zhang <kylezzhang@berkeley.edu>
2023-12-19 11:29:20 -0800

    Heat seeking missiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,28 +8,30 @@ public class Projectile : MonoBehaviour
     [SerializeField] private float movespeed;
     [SerializeField] private float duration;
 
-    private void Awake()
+    private Transform target;
+
+    private Transform SeekTarget(float radius, float dist, string layerName)
     {
+        var tf = transform;
+        var layerMask = 1 << LayerMask.NameToLayer(layerName);
+        var hitInfo = Physics2D.CircleCast(
+            tf.position, radius, tf.eulerAngles, dist, layerMask);
+        return hitInfo.transform;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (target)
+            transform.up = target.position - transform.position;
+        else
+            target = SeekTarget(13, 0, Enemy.LAYER_NAME);
+
         transform.position += transform.up * (Time.deltaTime * movespeed);
         duration -= Time.deltaTime;
-        var untilNextSec = duration - (int)duration;
         if (duration <= 0)
         {
             Destroy(gameObject);
-        }
-        // Close to 1 second mark or 0.5 second mark
-        else if (untilNextSec <= double.Epsilon ||
-                 (untilNextSec - 0.5 > 0 && untilNextSec - 0.5 <= double.Epsilon))
-        {
-            var radius = 5f;
-            // TODO: finish this
-            var hitInfo = Physics2D.CircleCast(transform.position, radius, transform.eulerAngles);
-
         }
     }
 
