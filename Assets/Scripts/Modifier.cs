9c62c668274f3021384ae5778757e93e5915bc52
Kyle Zhang <kylezzhang@berkeley.edu>
2024-07-01 19:58:30 -0700

    modifier TODO

---
+++ Assets/Scripts/Modifier.cs
@@ -0,0 +1,13 @@
+/*
+ * TODO: Define what a modifier can do
+ * Examples of upgrades that need modifiers
+ * GMP for projectiles
+ * Big projectiles
+ * Piercing projectiles
+ * Slow effect on hit for "frost peashooter"
+ *
+ * Should modifiers also apply to enemies?
+ */
+public class Modifier
+{
+}
